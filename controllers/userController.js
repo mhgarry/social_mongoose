5d21000fcfe6614b8e8c74a43b4e1c724b26dd54
Matt <mhgarry92@gmail.com>
2023-05-14 19:56:04 -0400

    user controller update

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,5 +35,37 @@ async createUser (req, res) {
 		console.log(err);
 		res.status(500).json(err)p;
 	}
-}
+},
+// update a user
+async updateUser (req, res) {
+	try {
+		const user = await User.findOneAndUpdate(
+			{ _id: req.params.id },
+			{ $set: req.body },
+			{ runValidators: true, new: true }
+		);
+	if (!user) {
+		return res.status(404).json({ message: 'No user with this id' });
+	}
+	res.json(user);
+	} catch(err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+},
+
+// delete a user and associated thoughts
+async deleteUser (req, res) {
+	try {
+		const user = await User.findOneAndDelete({ _id: req.params.userId });
+		if (!user) {
+			return res.status(404).json({ message: 'No user with this id'});
+		}
+	await Thought.deleteMany({ _id: { $in: user.thoughts } });
+	res.json({ message: 'User and thoughts deleted'});
+	} catch(err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+},
 }
