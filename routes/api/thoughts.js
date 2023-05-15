f63a988dd57e2a2667b4ceab63ddd01ca2d43286
Matt <mhgarry92@gmail.com>
2023-05-14 20:29:03 -0400

    finish thought routes

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -12,3 +12,15 @@ const {
 // get all thoughts
 router.route('/thoughts').get(getThoughts);
 router.post('/thoughts', createThought);
+// get one thought
+router.route('/thoughts/:id').get(getOneThought);
+// update thought
+router.route('/thoughts/:id').put(updateThought);
+// delete thought
+router.route('/thoughts/:id').delete(deleteThought);
+// add reaction
+router.route('/thoughts/:id/reactions').post(addReaction);
+// remove reaction
+router.route('/thoughts/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
